ab638c2204e1e892fdc02f6b538d7c24664b9e3f
kgtkr <kgtkr.jp@gmail.com>
2020-10-25 17:37:09 +0900

    バグ修正

--- src/app.ts
+++ src/app.ts
@@ -5,29 +5,53 @@ const client = new Discord.Client();
 
 client.on("ready", () => {});
 
-client.on("messageReactionAdd", async (msgReaction) => {
+client.on("raw" as any, async (packet) => {
   try {
-    const unpinReaction = msgReaction.message.reactions.cache
+    if (
+      !(
+        packet.t === "MESSAGE_REACTION_ADD" ||
+        packet.t === "MESSAGE_REACTION_REMOVE"
+      )
+    ) {
+      return;
+    }
+
+    const data: {
+      channel_id: string;
+      message_id: string;
+      emoji: {
+        name: string;
+      };
+      user_id: string;
+    } = packet.d;
+
+    if (!(data.emoji.name === "pin" || data.emoji.name === "unpin")) {
+      return;
+    }
+
+    const channel = await client.channels.fetch(data.channel_id, true);
+
+    if (!(channel instanceof Discord.TextChannel)) {
+      return;
+    }
+
+    const message = await channel.messages.fetch(data.message_id);
+
+    const unpinReaction = message.reactions.cache
       .array()
       .find((reaction) => reaction.emoji.name === "unpin");
 
     const unpin =
-      ((await unpinReaction?.fetch())?.users.resolve(
-        msgReaction.message.author
-      ) ?? null) !== null;
+      unpinReaction !== undefined &&
+      (await unpinReaction.fetch()).users.resolveID(message.author) !== null;
 
-    if (
-      msgReaction.emoji.name === "pin" &&
-      !unpin &&
-      !msgReaction.message.pinned
-    ) {
-      await msgReaction.message.pin();
+    if (data.emoji.name === "pin" && !unpin && !message.pinned) {
+      await message.pin();
       return;
     }
 
-    if (unpin && msgReaction.message.pinned) {
-      await msgReaction.message.unpin();
-
+    if (unpin && message.pinned) {
+      await message.unpin();
       return;
     }
   } catch (e) {
